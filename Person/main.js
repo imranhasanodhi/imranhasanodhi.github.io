52c5d7fe15cb14c07a9065dc26c7a0b0d4fd2dc8
Hasan Imran <s2401408@BC-RDHQJYJYGQ-Kannettava-tietokone.local>
2024-10-10 12:37:15 +0300

    Adding e

--- Person/main.js
+++ Person/main.js
@@ -1,25 +1,30 @@
-const names = document.querySelectorAll('#name')
-const usernames = document.querySelectorAll('#username')
-const emails = document.querySelectorAll('#email')
-const imgs = document.querySelectorAll('#img')
 
+const names = document.querySelectorAll('#name');
+const usernames = document.querySelectorAll('#username');
+const emails = document.querySelectorAll('#email');
+const imgs = document.querySelectorAll('#img');
 
-let userUrl = "https://jsonplaceholder.typicode.com/users"
-
-
+//API URL to fetch user data.
+let userUrl = "https://jsonplaceholder.typicode.com/users";
 
 const getData = async () => {
+    // Fetch data from the user API and wait for the response.
     let response = await fetch(userUrl);
+    
+    // Parse the JSON data from the response into a JavaScript object.
     let users = await response.json();
-    for (let i = 0; i < 10; i++) {
-        names[i].innerHTML = users[i].name
-        usernames[i].innerHTML = users[i].username
-        emails[i].innerHTML = users[i].email
-        let robUrl = `https://robohash.org/${i}`
-        imgs[i].src = robUrl
+    
 
+    for (let i = 0; i < 10; i++) {
+        names[i].innerHTML = users[i].name;
+        usernames[i].innerHTML = users[i].username;
+        emails[i].innerHTML = users[i].email;
+
+        // Generate a unique URL for a robot avatar based on the user's index.
+        let robUrl = `https://robohash.org/${i}`;
+        
+        // Set the src attribute of the 'img' elements.
+        imgs[i].src = robUrl;
     }
 }
-
-
-getData()
+getData();
